2b39c179cd3b7e297723dbd63a9bfd6e8d9dc5d2
slig2008 <slig@online.ms>
2026-03-03 18:15:20 +0000

    Make sure Git is initialize in devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,4 +4,7 @@ apt-get install -y --no-install-recommends git ssh curl unzip zsh bat
 
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin latest
 
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USER"
+
 go install -v github.com/go-delve/delve/cmd/dlv@latest
