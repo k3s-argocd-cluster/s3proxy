1141b841d05d53a24b92db42cdee13a67f9f77f1
slig2008 <slig@online.ms>
2026-03-03 20:13:53 +0000

    Helm chart fine tuning and 3.0.0 preparation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,9 @@
 apt-get update
 apt-get install -y --no-install-recommends git ssh curl unzip zsh bat
 
+cd /tmp
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin latest
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | bash
 
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER"
